eef02a74805c857f25b1ff6403ab96c181d623da
Tee1902 <kietta3377@ut.edu.vn>
2025-11-12 01:40:25 +0700

    program -client

--- CaroGame/Client/Program.cs
+++ CaroGame/Client/Program.cs
@@ -1,16 +1,15 @@
-namespace Client;
+using System;
+using System.Windows.Forms;
 
-static class Program
+namespace Client
 {
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main()
+    internal static class Program
     {
-        // To customize application configuration such as set high DPI settings or default font,
-        // see https://aka.ms/applicationconfiguration.
-        ApplicationConfiguration.Initialize();
-        Application.Run(new Form1());
-    }    
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MainForm());
+        }
+    }
 }
